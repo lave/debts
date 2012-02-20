5eb995fdd8ed64d8d25355367d8b06bd7402974a
Anton Lavrov <lavrov.anton@gmail.com>
2012-02-21 00:16:58 +0400

    added more lexer tests

--- testsuite/tests/LexerTest.hs
+++ testsuite/tests/LexerTest.hs
@@ -6,10 +6,43 @@ import Lexer
 
 
 parseTests = test [
+    "symbols" ~:
+        map TokenSym allowedSymbols
+        ~=? scan allowedSymbols,
+
+    "symbols" ~:
+        map TokenKeyword allowedKeywords
+        ~=? scan (unwords allowedKeywords),
+
+    "numbers" ~:
+        [TokenNumber 0,
+         TokenNumber 0,
+         TokenNumber 12,
+         TokenNumber (-34),
+         TokenNumber 0.0,
+         TokenNumber 0.0,
+         TokenNumber 12.3,
+         TokenNumber (-45.6)]
+        ~=? scan "0 -0 12 -34 0.0 -0.0 12.3 -45.6",
+
+    "strings" ~:
+        [TokenString "abc",
+         TokenString "def ghi"]
+        ~=? scan "abc \"def ghi\"",
+
     "whitespaces" ~:
         [TokenString "a"]
-        ~=? alexScanTokens " a  "
+        ~=? scan " a  ",
+
+    "comments" ~:
+        [TokenString "a"]
+        ~=? scan "a#bc"
+
     ]
+    where
+        scan = (map token_data) . alexScanTokens
+        allowedSymbols = ">:,_*=-@()[]"
+        allowedKeywords = ["param", "fx", "group", "date", "internal"]
 
 tests = test [parseTests]
 
