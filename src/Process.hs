186e33836c7ad270ff81b05378687684e05aa809
Anton Lavrov <lavrov.anton@gmail.com>
2013-12-06 03:56:33 +0400

    fix print results when some side has no expenses

--- src/Process.hs
+++ src/Process.hs
@@ -6,6 +6,7 @@ import qualified Data.List as List
 import BasicTypes
 import Balance
 import CommonCalculationLog
+import Money
 import MoneyLog
 import qualified Result
 import Side
@@ -22,13 +23,22 @@ data Operation =
 process :: Operation -> Transactions -> Result.Result
 
 process Balance transactions =
-    Result.Balance $ zipWith zipper balance' expenses'
+    Result.Balance $ zip balance' expenses'
     where
         balance'  = List.sort $ balance transactions
         expenses' = List.sort $ expenses transactions
 
-        zipper (Side name1 balance) (Side name2 expenses)
-            | name1 == name2 = (name1, balance, expenses)
+        na = Moneys []
+
+        zip [] [] = []
+        zip [] (Side name expenses : sides) = (name, na, expenses) : zip [] sides
+        zip (Side name balance : sides) [] = (name, balance, na) : zip sides []
+        zip s1@(Side name1 balance : sides1) s2@(Side name2 expenses : sides2)
+            | comparision == EQ = (name1, balance, expenses) : zip sides1 sides2
+            | comparision == LT = (name1, balance, na) : zip sides1 s2
+            | comparision == GT = (name2, na, expenses) : zip s1 sides2
+            where
+                comparision = compare name1 name2
 
 
 process CommonCalculationLog transactions =
