520dab339d350cebc2cb5f021127426bf772b74f
Anton Lavrov <anton.lavrov@oracle.com>
2022-09-03 13:20:42 -0700

    fixed script to test all examples for correctness

--- test-examples.sh
+++ test-examples.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find doc/examples/ -type f | while read f; do ( ./dist/build/debts/debts "$f" > /dev/null 2>&1 ) || echo "FAILED: $f"; done
+find doc/examples/ -type f | while read f; do ( ./debts "$f" > /dev/null 2>&1 ) || echo "FAILED: $f"; done
